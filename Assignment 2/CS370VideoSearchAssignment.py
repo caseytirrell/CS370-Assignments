4dd83c267ca8473fc1a04a5a86d03ca844deebb5
Casey Tirrell <tirrellcasey@gmail.com>
2024-02-27 23:27:26 -0500

    Worked on object detection and storage back into postgreSQL

--- Assignment 2/CS370VideoSearchAssignment.py
+++ Assignment 2/CS370VideoSearchAssignment.py
@@ -11,38 +11,173 @@ import os
 import uuid
 from urllib.error import URLError
 
-
-
 model = ResNet50(weights='imagenet', include_top=False)
+
+#CNN Model
+modelURL = "https://tfhub.dev/tensorflow/ssd_mobilenet_v2/fpnlite_320x320/1"
+detector = tfhub.load(modelURL)
+
+IDtoClassName = {
+    1: "person",
+    2: "bicycle",
+    3: "car",
+    4: "motorcycle",
+    5: "airplane",
+    6: "bus",
+    7: "train",
+    8: "truck",
+    9: "boat",
+    10: "traffic light",
+    11: "fire hydrant",
+    13: "stop sign",
+    14: "parking meter",
+    15: "bench",
+    16: "bird",
+    17: "cat",
+    18: "dog",
+    19: "horse",
+    20: "sheep",
+    21: "cow",
+    22: "elephant",
+    23: "bear",
+    24: "zebra",
+    25: "giraffe",
+    27: "backpack",
+    28: "umbrella",
+    31: "handbag",
+    32: "tie",
+    33: "suitcase",
+    34: "frisbee",
+    35: "skis",
+    36: "snowboard",
+    37: "sports ball",
+    38: "kite",
+    39: "baseball bat",
+    40: "baseball glove",
+    41: "skateboard",
+    42: "surfboard",
+    43: "tennis racket",
+    44: "bottle",
+    46: "wine glass",
+    47: "cup",
+    48: "fork",
+    49: "knife",
+    50: "spoon",
+    51: "bowl",
+    52: "banana",
+    53: "apple",
+    54: "sandwich",
+    55: "orange",
+    56: "broccoli",
+    57: "carrot",
+    58: "hot dog",
+    59: "pizza",
+    60: "donut",
+    61: "cake",
+    62: "chair",
+    63: "couch",
+    64: "potted plant",
+    65: "bed",
+    67: "dining table",
+    70: "toilet",
+    72: "tv",
+    73: "laptop",
+    74: "mouse",
+    75: "remote",
+    76: "keyboard",
+    77: "cell phone",
+    78: "microwave",
+    79: "oven",
+    80: "toaster",
+    81: "sink",
+    82: "refrigerator",
+    84: "book",
+    85: "clock",
+    86: "vase",
+    87: "scissors",
+    88: "teddy bear",
+    89: "hair drier",
+    90: "toothbrush",
+}
+
+
+def detectObjects(frame, detector):
+
+    convertedResult = {}
+    detections = []
+    
+    #Convert to the right format
+    convertedImg = tf.image.convert_image_dtype(frame, tf.float32)[tf.newaxis, ...]
+    result = detector(convertedImg)
+
+    numDetections = int(result["num_detections"].numpy())
+    boundingBox = result["detection_boxes"].numpy()[0]
+    ID = result["detection_classes"].numpy()[0]
+    confidenceScore = result["detection_scores"].numpy()[0]
+
+    #detection_scores is returned by the R-CNN model and represents a confidence level for the detection
+    for i in range(numDetections):
+        #Threshold set to 50%
+        if confidenceScore[i] >= 0.5:
+                score = confidenceScore[i]
+                bbox = boundingBox[i]
+                classID = int(ID[i])
+                className = IDtoClassName.get(classID, "Unknown")
+
+                detections.append({
+                    'classID': classID,
+                    'className': className,
+                    'bbox': bbox,
+                    'score': score
+                })            
+    return detections    
+
+def storeDetections(videoID, frameData, dbParams):
+    conn = psycopg2.connect(**dbParams)
+    cursor = conn.cursor()
+
+    for frame, (timestamp, detections) in enumerate(frameData):
+        for detection in detections:
+            score = detection['score']
+            bbox = detection['bbox']
+            classID = detection['classID']
+            className = IDtoClassName.get(classID, "Unknown")
+
+            cursor.execute(
+                "INSERT INTO detectedObjects (vidId, frameNum, timestamp, detectedObjId, detectedObjClass, confidence, bbox) VALUES (%s, %s, %s, %s, %s, %s, %s,)"
+                (videoID, frame, timestamp, classID, className, score, bbox)
+            )
+    conn.commit()
+    cursor.close()
+    conn.close()
+    print(f"Object Detection for for video {videoID} successfully saved to the database.")
 
 def genEmbeddings(frames):
     embeddings = []
+    #preprocessing
     for frame in frames:
+        #correcting color mode
         img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        #resizing
         img = cv2.resize(img, (224, 224))
+        #convert to numpy array
         img = image.img_to_array(img)
+        #Reshaping
         img = np.expand_dims(img, axis=0)
+        #normalization and scaling
         img = preprocess_input(img)
 
         embedding = model.predict(img)
         embeddings.append(embedding.flatten())
     return embeddings
-''''
-def storeEmbeddings(videoID, embeddings, outputPath):
-    vDirectory = os.path.join(outputPath, videoID)
-    if not os.path.exists(vDirectory):
-        os.makedirs(vDirectory)
-    embeddingsFile = os.path.join(vDirectory, 'frames.npy')
-    np.save(embeddingsFile, np.array(embeddings))
-    print(f"Embeddings successfully saved to {embeddingsFile}")
-'''
+
 def storeEmbeddings(videoID, embeddings, dbParams):
     conn = psycopg2.connect(**dbParams)
     cursor = conn.cursor()
     for i, j in enumerate(embeddings):
         embeddingList = j.tolist()
         cursor.execute(
-            "INSERT INTO videoembeddings (videoID, frameNumber, embedding) VALUES (%s, %s, %s)",
+            "INSERT INTO vidembeddings (videoID, frameNumber, embedding) VALUES (%s, %s, %s)",
             (videoID, i, embeddingList)
         )
     conn.commit()
@@ -64,9 +199,19 @@ def extractFrames(videoPath, interval=1):
     return frames
 
 def process_video(videoPath, videoID, dbParams, interval=1):
+    frameData = []
+
     frames = extractFrames(videoPath, interval)
     embeddings = genEmbeddings(frames)
     storeEmbeddings(videoID, embeddings, dbParams)
+
+    for i, frame in enumerate(frames):
+        detections = detectObjects(frame, detector)
+        # Assuming you have a timestamp or can calculate it
+        timestamp = i * interval  # Placeholder for actual timestamp calculation
+        frameData.append((timestamp, detections))
+    storeDetections(videoID, frameData, dbParams)
+
 
 def download(url, outputPath):   
     try:
